de8ff2252e4fa661e2dd7afa8a91f020d6300f8b
jmorallos <jmorallos@github.com>
2026-05-29 19:06:22 +0800

    add preventDefault

---
+++ preventDefault/index.js
@@ -0,0 +1,21 @@
+// const input = document.querySelector("input");
+// const output = document.getElementById("output");
+
+// input.addEventListener("keydown", (e) => {
+//     output.innerText = `You pressed the ${e.key} key`;
+// });
+
+const input = document.querySelector("input");
+const output = document.getElementById("output");
+
+input.addEventListener("keydown", (e) => {
+    e.preventDefault();
+    output.innerText = `You pressed the ${e.key} key`;
+});
+
+// const form = document.querySelector("form");
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     // rest of code goes here
+// });
